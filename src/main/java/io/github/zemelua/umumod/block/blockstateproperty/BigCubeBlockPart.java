27cb381a2a49182e459a7823e3d948d11352ef7a
Zemelua <red0165333@gmail.com>
2021-04-19 22:41:49 +0900

    Add big cube part Enum

---
+++ src/main/java/io/github/zemelua/umumod/block/blockstateproperty/BigCubeBlockPart.java
@@ -0,0 +1,31 @@
+package io.github.zemelua.umumod.block.blockstateproperty;
+
+import net.minecraft.util.IStringSerializable;
+
+public enum BigCubeBlockPart implements IStringSerializable {
+	UPPER_FRONT_RIGHT("upper_front_right"),
+	UPPER_FRONT_LEFT ("upper_front_left"),
+	UPPER_BACK_RIGHT ("upper_back_right"),
+	UPPER_BACK_LEFT  ("upper_back_left"),
+	LOWER_FRONT_RIGHT("lower_front_right"),
+	LOWER_FRONT_LEFT ("lower_front_left"),
+	LOWER_BACK_RIGHT ("lower_back_right"),
+	LOWER_BACK_LEFT  ("lower_back_left")
+	;
+
+	private String name;
+
+	private BigCubeBlockPart(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return this.getString();
+	}
+
+	@Override
+	public String getString() {
+		return this.name;
+	}
+}
--- src/main/java/io/github/zemelua/umumod/block/blockstateproperty/UMUBlockStateProperties.java
+++ src/main/java/io/github/zemelua/umumod/block/blockstateproperty/UMUBlockStateProperties.java
@@ -5,4 +5,5 @@ import net.minecraft.state.EnumProperty;
 public class UMUBlockStateProperties {
 	public static final EnumProperty<VerticalSlabType> VERTICAL_SLAB_TYPE = EnumProperty.create("type", VerticalSlabType.class);
 	public static final EnumProperty<ColumnType> COLUMN_TYPE = EnumProperty.create("type", ColumnType.class);
+	public static final EnumProperty<BigCubeBlockPart> BIG_CUBE_PART = EnumProperty.create("part", BigCubeBlockPart.class);
 }
