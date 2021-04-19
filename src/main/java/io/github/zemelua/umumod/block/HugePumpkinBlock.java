167cecf2a833ff7ea8a99d81284ea2f235cbc63d
Zemelua <red0165333@gmail.com>
2021-04-19 17:07:49 +0900

    Add huge pumpkin Class and related Class

---
+++ src/main/java/io/github/zemelua/umumod/block/HugePumpkinBlock.java
@@ -0,0 +1,9 @@
+package io.github.zemelua.umumod.block;
+
+import net.minecraft.block.HorizontalBlock;
+
+public class HugePumpkinBlock extends HorizontalBlock {
+    public HugePumpkinBlock(Properties builder) {
+        super(builder);
+    }
+}
