85df0fafeef4a5a7d1d1853d643980ea39b5b146
Zemelua <red0165333@gmail.com>
2021-04-19 22:07:03 +0900

    Remove huge pumpkin Enum

--- src/main/java/io/github/zemelua/umumod/block/HugePumpkinBlock.java
+++ src/main/java/io/github/zemelua/umumod/block/HugePumpkinBlock.java
@@ -3,7 +3,7 @@ package io.github.zemelua.umumod.block;
 import net.minecraft.block.HorizontalBlock;
 
 public class HugePumpkinBlock extends HorizontalBlock {
-    public HugePumpkinBlock(Properties builder) {
-        super(builder);
-    }
+	public HugePumpkinBlock(Properties builder) {
+		super(builder);
+	}
 }
