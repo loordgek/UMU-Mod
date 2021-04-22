0837fcb2d3d43e0395d752591bc526bfb5979f14
Zemelua <red0165333@gmail.com>
2021-04-22 15:25:44 +0900

    Remove color planks Blocks, BlockItems and doors
    Instead, implement a tree with each tint

--- src/main/java/io/github/zemelua/umumod/block/UMUBlocks.java
+++ src/main/java/io/github/zemelua/umumod/block/UMUBlocks.java
@@ -4,7 +4,6 @@ import io.github.zemelua.umumod.UMUMod;
 import io.github.zemelua.umumod.block.blockstateproperty.ColumnBlock;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
-import net.minecraft.block.DoorBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
@@ -15,46 +14,12 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class UMUBlocks {
 	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, UMUMod.MODID);
 
-	public static final RegistryObject<Block> WHITE_PLANKS = BLOCKS.register("white_planks", () -> new Block(Block.Properties.create(Material.WOOD, MaterialColor.SNOW).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD)));
-	public static final RegistryObject<Block> ORANGE_PLANKS = BLOCKS.register("orange_planks", () -> new Block(Block.Properties.create(Material.WOOD, MaterialColor.ADOBE).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD)));
-	public static final RegistryObject<Block> MAGENTA_PLANKS = BLOCKS.register("magenta_planks", () -> new Block(Block.Properties.create(Material.WOOD, MaterialColor.MAGENTA).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD)));
-	public static final RegistryObject<Block> LIGHT_BLUE_PLANKS = BLOCKS.register("light_blue_planks", () -> new Block(Block.Properties.create(Material.WOOD, MaterialColor.LIGHT_BLUE).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD)));
-	public static final RegistryObject<Block> YELLOW_PLANKS = BLOCKS.register("yellow_planks", () -> new Block(Block.Properties.create(Material.WOOD, MaterialColor.YELLOW).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD)));
-	public static final RegistryObject<Block> LIME_PLANKS = BLOCKS.register("lime_planks", () -> new Block(Block.Properties.create(Material.WOOD, MaterialColor.LIME).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD)));
-	public static final RegistryObject<Block> PINK_PLANKS = BLOCKS.register("pink_planks", () -> new Block(Block.Properties.create(Material.WOOD, MaterialColor.PINK).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD)));
-	public static final RegistryObject<Block> GRAY_PLANKS = BLOCKS.register("gray_planks", () -> new Block(Block.Properties.create(Material.WOOD, MaterialColor.GRAY).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD)));
-	public static final RegistryObject<Block> LIGHT_GRAY_PLANKS = BLOCKS.register("light_gray_planks", () -> new Block(Block.Properties.create(Material.WOOD, MaterialColor.LIGHT_GRAY).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD)));
-	public static final RegistryObject<Block> CYAN_PLANKS = BLOCKS.register("cyan_planks", () -> new Block(Block.Properties.create(Material.WOOD, MaterialColor.CYAN).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD)));
-	public static final RegistryObject<Block> PURPLE_PLANKS = BLOCKS.register("purple_planks", () -> new Block(Block.Properties.create(Material.WOOD, MaterialColor.PURPLE).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD)));
-	public static final RegistryObject<Block> BLUE_PLANKS = BLOCKS.register("blue_planks", () -> new Block(Block.Properties.create(Material.WOOD, MaterialColor.BLUE).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD)));
-	public static final RegistryObject<Block> BROWN_PLANKS = BLOCKS.register("brown_planks", () -> new Block(Block.Properties.create(Material.WOOD, MaterialColor.BROWN).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD)));
-	public static final RegistryObject<Block> GREEN_PLANKS = BLOCKS.register("green_planks", () -> new Block(Block.Properties.create(Material.WOOD, MaterialColor.GREEN).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD)));
-	public static final RegistryObject<Block> RED_PLANKS = BLOCKS.register("red_planks", () -> new Block(Block.Properties.create(Material.WOOD, MaterialColor.RED).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD)));
-	public static final RegistryObject<Block> BLACK_PLANKS = BLOCKS.register("black_planks", () -> new Block(Block.Properties.create(Material.WOOD, MaterialColor.BLACK).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD)));
-
 	public static final RegistryObject<Block> OAK_VERTICAL_SLAB = BLOCKS.register("oak_vertical_slab", () -> new VerticalSlabBlock(AbstractBlock.Properties.create(Material.WOOD, MaterialColor.WOOD).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD)));
 	public static final RegistryObject<Block> SPRUCE_VERTICAL_SLAB = BLOCKS.register("spruce_vertical_slab", () -> new VerticalSlabBlock(AbstractBlock.Properties.create(Material.WOOD, MaterialColor.OBSIDIAN).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD)));
 	public static final RegistryObject<Block> BIRCH_VERTICAL_SLAB = BLOCKS.register("birch_vertical_slab", () -> new VerticalSlabBlock(AbstractBlock.Properties.create(Material.WOOD, MaterialColor.SAND).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD)));
 	public static final RegistryObject<Block> JUNGLE_VERTICAL_SLAB = BLOCKS.register("jungle_vertical_slab", () -> new VerticalSlabBlock(AbstractBlock.Properties.create(Material.WOOD, MaterialColor.DIRT).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD)));
 	public static final RegistryObject<Block> ACACIA_VERTICAL_SLAB = BLOCKS.register("acacia_vertical_slab", () -> new VerticalSlabBlock(AbstractBlock.Properties.create(Material.WOOD, MaterialColor.ADOBE).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD)));
 	public static final RegistryObject<Block> DARK_OAK_VERTICAL_SLAB = BLOCKS.register("dark_oak_vertical_slab", () -> new VerticalSlabBlock(AbstractBlock.Properties.create(Material.WOOD, MaterialColor.BROWN).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD)));
-
-	public static final RegistryObject<Block> WHITE_WOODEN_DOOR = BLOCKS.register("white_wooden_door", () -> new DoorBlock(AbstractBlock.Properties.create(Material.WOOD, WHITE_PLANKS.get().getMaterialColor()).hardnessAndResistance(3.0F).sound(SoundType.WOOD).notSolid()));
-	public static final RegistryObject<Block> ORANGE_WOODEN_DOOR = BLOCKS.register("orange_wooden_door", () -> new DoorBlock(AbstractBlock.Properties.create(Material.WOOD, ORANGE_PLANKS.get().getMaterialColor()).hardnessAndResistance(3.0F).sound(SoundType.WOOD).notSolid()));
-	public static final RegistryObject<Block> MAGENTA_WOODEN_DOOR = BLOCKS.register("magenta_wooden_door", () -> new DoorBlock(AbstractBlock.Properties.create(Material.WOOD, MAGENTA_PLANKS.get().getMaterialColor()).hardnessAndResistance(3.0F).sound(SoundType.WOOD).notSolid()));
-	public static final RegistryObject<Block> LIGHT_BLUE_WOODEN_DOOR = BLOCKS.register("light_blue_wooden_door", () -> new DoorBlock(AbstractBlock.Properties.create(Material.WOOD, LIGHT_BLUE_PLANKS.get().getMaterialColor()).hardnessAndResistance(3.0F).sound(SoundType.WOOD).notSolid()));
-	public static final RegistryObject<Block> YELLOW_WOODEN_DOOR = BLOCKS.register("yellow_wooden_door", () -> new DoorBlock(AbstractBlock.Properties.create(Material.WOOD, YELLOW_PLANKS.get().getMaterialColor()).hardnessAndResistance(3.0F).sound(SoundType.WOOD).notSolid()));
-	public static final RegistryObject<Block> LIME_WOODEN_DOOR = BLOCKS.register("lime_wooden_door", () -> new DoorBlock(AbstractBlock.Properties.create(Material.WOOD, LIME_PLANKS.get().getMaterialColor()).hardnessAndResistance(3.0F).sound(SoundType.WOOD).notSolid()));
-	public static final RegistryObject<Block> PINK_WOODEN_DOOR = BLOCKS.register("pink_wooden_door", () -> new DoorBlock(AbstractBlock.Properties.create(Material.WOOD, PINK_PLANKS.get().getMaterialColor()).hardnessAndResistance(3.0F).sound(SoundType.WOOD).notSolid()));
-	public static final RegistryObject<Block> GRAY_WOODEN_DOOR = BLOCKS.register("gray_wooden_door", () -> new DoorBlock(AbstractBlock.Properties.create(Material.WOOD, GRAY_PLANKS.get().getMaterialColor()).hardnessAndResistance(3.0F).sound(SoundType.WOOD).notSolid()));
-	public static final RegistryObject<Block> LIGHT_GRAY_WOODEN_DOOR = BLOCKS.register("light_gray_wooden_door", () -> new DoorBlock(AbstractBlock.Properties.create(Material.WOOD, LIGHT_GRAY_PLANKS.get().getMaterialColor()).hardnessAndResistance(3.0F).sound(SoundType.WOOD).notSolid()));
-	public static final RegistryObject<Block> CYAN_WOODEN_DOOR = BLOCKS.register("cyan_wooden_door", () -> new DoorBlock(AbstractBlock.Properties.create(Material.WOOD, CYAN_PLANKS.get().getMaterialColor()).hardnessAndResistance(3.0F).sound(SoundType.WOOD).notSolid()));
-	public static final RegistryObject<Block> PURPLE_WOODEN_DOOR = BLOCKS.register("purple_wooden_door", () -> new DoorBlock(AbstractBlock.Properties.create(Material.WOOD, PURPLE_PLANKS.get().getMaterialColor()).hardnessAndResistance(3.0F).sound(SoundType.WOOD).notSolid()));
-	public static final RegistryObject<Block> BLUE_WOODEN_DOOR = BLOCKS.register("blue_wooden_door", () -> new DoorBlock(AbstractBlock.Properties.create(Material.WOOD, BLUE_PLANKS.get().getMaterialColor()).hardnessAndResistance(3.0F).sound(SoundType.WOOD).notSolid()));
-	public static final RegistryObject<Block> BROWN_WOODEN_DOOR = BLOCKS.register("brown_wooden_door", () -> new DoorBlock(AbstractBlock.Properties.create(Material.WOOD, BROWN_PLANKS.get().getMaterialColor()).hardnessAndResistance(3.0F).sound(SoundType.WOOD).notSolid()));
-	public static final RegistryObject<Block> GREEN_WOODEN_DOOR = BLOCKS.register("green_wooden_door", () -> new DoorBlock(AbstractBlock.Properties.create(Material.WOOD, GREEN_PLANKS.get().getMaterialColor()).hardnessAndResistance(3.0F).sound(SoundType.WOOD).notSolid()));
-	public static final RegistryObject<Block> RED_WOODEN_DOOR = BLOCKS.register("red_wooden_door", () -> new DoorBlock(AbstractBlock.Properties.create(Material.WOOD, RED_PLANKS.get().getMaterialColor()).hardnessAndResistance(3.0F).sound(SoundType.WOOD).notSolid()));
-	public static final RegistryObject<Block> BLACK_WOODEN_DOOR = BLOCKS.register("black_wooden_door", () -> new DoorBlock(AbstractBlock.Properties.create(Material.WOOD, BLACK_PLANKS.get().getMaterialColor()).hardnessAndResistance(3.0F).sound(SoundType.WOOD).notSolid()));
 
 	public static final RegistryObject<Block> WOODEN_BOX = BLOCKS.register("wooden_box", () -> new WoodenBoxBlock(AbstractBlock.Properties.create(Material.WOOD, MaterialColor.WOOD).hardnessAndResistance(4.5F).sound(SoundType.WOOD)));
 	public static final RegistryObject<Block> QUARTZ_COLUMN = BLOCKS.register("quartz_column", () -> new ColumnBlock(AbstractBlock.Properties.create(Material.ROCK, MaterialColor.QUARTZ).hardnessAndResistance(0.8F)));
--- src/main/java/io/github/zemelua/umumod/item/UMUItems.java
+++ src/main/java/io/github/zemelua/umumod/item/UMUItems.java
@@ -12,46 +12,12 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class UMUItems {
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, UMUMod.MODID);
 
-	public static final RegistryObject<Item> WHITE_PLANKS = ITEMS.register("white_planks", () -> new BlockItem(UMUBlocks.WHITE_PLANKS.get(), new Item.Properties().group(UMUItemGroups.BUILDING_BLOCKS)));
-	public static final RegistryObject<Item> ORANGE_PLANKS = ITEMS.register("orange_planks", () -> new BlockItem(UMUBlocks.ORANGE_PLANKS.get(), new Item.Properties().group(UMUItemGroups.BUILDING_BLOCKS)));
-	public static final RegistryObject<Item> MAGENTA_PLANKS = ITEMS.register("magenta_planks", () -> new BlockItem(UMUBlocks.MAGENTA_PLANKS.get(), new Item.Properties().group(UMUItemGroups.BUILDING_BLOCKS)));
-	public static final RegistryObject<Item> LIGHT_BLUE_PLANKS = ITEMS.register("light_blue_planks", () -> new BlockItem(UMUBlocks.LIGHT_BLUE_PLANKS.get(), new Item.Properties().group(UMUItemGroups.BUILDING_BLOCKS)));
-	public static final RegistryObject<Item> YELLOW_PLANKS = ITEMS.register("yellow_planks", () -> new BlockItem(UMUBlocks.YELLOW_PLANKS.get(), new Item.Properties().group(UMUItemGroups.BUILDING_BLOCKS)));
-	public static final RegistryObject<Item> LIME_PLANKS = ITEMS.register("lime_planks", () -> new BlockItem(UMUBlocks.LIME_PLANKS.get(), new Item.Properties().group(UMUItemGroups.BUILDING_BLOCKS)));
-	public static final RegistryObject<Item> PINK_PLANKS = ITEMS.register("pink_planks", () -> new BlockItem(UMUBlocks.PINK_PLANKS.get(), new Item.Properties().group(UMUItemGroups.BUILDING_BLOCKS)));
-	public static final RegistryObject<Item> GRAY_PLANKS = ITEMS.register("gray_planks", () -> new BlockItem(UMUBlocks.GRAY_PLANKS.get(), new Item.Properties().group(UMUItemGroups.BUILDING_BLOCKS)));
-	public static final RegistryObject<Item> LIGHT_GRAY_PLANKS = ITEMS.register("light_gray_planks", () -> new BlockItem(UMUBlocks.LIGHT_GRAY_PLANKS.get(), new Item.Properties().group(UMUItemGroups.BUILDING_BLOCKS)));
-	public static final RegistryObject<Item> CYAN_PLANKS = ITEMS.register("cyan_planks", () -> new BlockItem(UMUBlocks.CYAN_PLANKS.get(), new Item.Properties().group(UMUItemGroups.BUILDING_BLOCKS)));
-	public static final RegistryObject<Item> PURPLE_PLANKS = ITEMS.register("purple_planks", () -> new BlockItem(UMUBlocks.PURPLE_PLANKS.get(), new Item.Properties().group(UMUItemGroups.BUILDING_BLOCKS)));
-	public static final RegistryObject<Item> BLUE_PLANKS = ITEMS.register("blue_planks", () -> new BlockItem(UMUBlocks.BLUE_PLANKS.get(), new Item.Properties().group(UMUItemGroups.BUILDING_BLOCKS)));
-	public static final RegistryObject<Item> BROWN_PLANKS = ITEMS.register("brown_planks", () -> new BlockItem(UMUBlocks.BROWN_PLANKS.get(), new Item.Properties().group(UMUItemGroups.BUILDING_BLOCKS)));
-	public static final RegistryObject<Item> GREEN_PLANKS = ITEMS.register("green_planks", () -> new BlockItem(UMUBlocks.GREEN_PLANKS.get(), new Item.Properties().group(UMUItemGroups.BUILDING_BLOCKS)));
-	public static final RegistryObject<Item> RED_PLANKS = ITEMS.register("red_planks", () -> new BlockItem(UMUBlocks.RED_PLANKS.get(), new Item.Properties().group(UMUItemGroups.BUILDING_BLOCKS)));
-	public static final RegistryObject<Item> BLACK_PLANKS = ITEMS.register("black_planks", () -> new BlockItem(UMUBlocks.BLACK_PLANKS.get(), new Item.Properties().group(UMUItemGroups.BUILDING_BLOCKS)));
-
 	public static final RegistryObject<Item> OAK_VERTICAL_SLAB = ITEMS.register("oak_vertical_slab", () -> new BlockItem(UMUBlocks.OAK_VERTICAL_SLAB.get(), new Item.Properties().group(UMUItemGroups.BUILDING_BLOCKS)));
 	public static final RegistryObject<Item> SPRUCE_VERTICAL_SLAB = ITEMS.register("spruce_vertical_slab", () -> new BlockItem(UMUBlocks.SPRUCE_VERTICAL_SLAB.get(), new Item.Properties().group(UMUItemGroups.BUILDING_BLOCKS)));
 	public static final RegistryObject<Item> BIRCH_VERTICAL_SLAB = ITEMS.register("birch_vertical_slab", () -> new BlockItem(UMUBlocks.BIRCH_VERTICAL_SLAB.get(), new Item.Properties().group(UMUItemGroups.BUILDING_BLOCKS)));
 	public static final RegistryObject<Item> JUNGLE_VERTICAL_SLAB = ITEMS.register("jungle_vertical_slab", () -> new BlockItem(UMUBlocks.JUNGLE_VERTICAL_SLAB.get(), new Item.Properties().group(UMUItemGroups.BUILDING_BLOCKS)));
 	public static final RegistryObject<Item> ACACIA_VERTICAL_SLAB = ITEMS.register("acacia_vertical_slab", () -> new BlockItem(UMUBlocks.ACACIA_VERTICAL_SLAB.get(), new Item.Properties().group(UMUItemGroups.BUILDING_BLOCKS)));
 	public static final RegistryObject<Item> DARK_OAK_VERTICAL_SLAB = ITEMS.register("dark_oak_vertical_slab", () -> new BlockItem(UMUBlocks.DARK_OAK_VERTICAL_SLAB.get(), new Item.Properties().group(UMUItemGroups.BUILDING_BLOCKS)));
-
-	public static final RegistryObject<Item> WHITE_WOODEN_DOOR = ITEMS.register("white_wooden_door", () -> new BlockItem(UMUBlocks.WHITE_WOODEN_DOOR.get(), new Item.Properties().group(UMUItemGroups.FURNITURE)));
-	public static final RegistryObject<Item> ORANGE_WOODEN_DOOR = ITEMS.register("orange_wooden_door", () -> new BlockItem(UMUBlocks.ORANGE_WOODEN_DOOR.get(), new Item.Properties().group(UMUItemGroups.FURNITURE)));
-	public static final RegistryObject<Item> MAGENTA_WOODEN_DOOR = ITEMS.register("magenta_wooden_door", () -> new BlockItem(UMUBlocks.MAGENTA_WOODEN_DOOR.get(), new Item.Properties().group(UMUItemGroups.FURNITURE)));
-	public static final RegistryObject<Item> LIGHT_BLUE_WOODEN_DOOR = ITEMS.register("light_blue_wooden_door", () -> new BlockItem(UMUBlocks.LIGHT_BLUE_WOODEN_DOOR.get(), new Item.Properties().group(UMUItemGroups.FURNITURE)));
-	public static final RegistryObject<Item> YELLOW_WOODEN_DOOR = ITEMS.register("yellow_wooden_door", () -> new BlockItem(UMUBlocks.YELLOW_WOODEN_DOOR.get(), new Item.Properties().group(UMUItemGroups.FURNITURE)));
-	public static final RegistryObject<Item> LIME_WOODEN_DOOR = ITEMS.register("lime_wooden_door", () -> new BlockItem(UMUBlocks.LIME_WOODEN_DOOR.get(), new Item.Properties().group(UMUItemGroups.FURNITURE)));
-	public static final RegistryObject<Item> PINK_WOODEN_DOOR = ITEMS.register("pink_wooden_door", () -> new BlockItem(UMUBlocks.PINK_WOODEN_DOOR.get(), new Item.Properties().group(UMUItemGroups.FURNITURE)));
-	public static final RegistryObject<Item> GRAY_WOODEN_DOOR = ITEMS.register("gray_wooden_door", () -> new BlockItem(UMUBlocks.GRAY_WOODEN_DOOR.get(), new Item.Properties().group(UMUItemGroups.FURNITURE)));
-	public static final RegistryObject<Item> LIGHT_GRAY_WOODEN_DOOR = ITEMS.register("light_gray_wooden_door", () -> new BlockItem(UMUBlocks.LIGHT_GRAY_WOODEN_DOOR.get(), new Item.Properties().group(UMUItemGroups.FURNITURE)));
-	public static final RegistryObject<Item> CYAN_WOODEN_DOOR = ITEMS.register("cyan_wooden_door", () -> new BlockItem(UMUBlocks.CYAN_WOODEN_DOOR.get(), new Item.Properties().group(UMUItemGroups.FURNITURE)));
-	public static final RegistryObject<Item> PURPLE_WOODEN_DOOR = ITEMS.register("purple_wooden_door", () -> new BlockItem(UMUBlocks.PURPLE_WOODEN_DOOR.get(), new Item.Properties().group(UMUItemGroups.FURNITURE)));
-	public static final RegistryObject<Item> BLUE_WOODEN_DOOR = ITEMS.register("blue_wooden_door", () -> new BlockItem(UMUBlocks.BLUE_WOODEN_DOOR.get(), new Item.Properties().group(UMUItemGroups.FURNITURE)));
-	public static final RegistryObject<Item> BROWN_WOODEN_DOOR = ITEMS.register("brown_wooden_door", () -> new BlockItem(UMUBlocks.BROWN_WOODEN_DOOR.get(), new Item.Properties().group(UMUItemGroups.FURNITURE)));
-	public static final RegistryObject<Item> GREEN_WOODEN_DOOR = ITEMS.register("green_wooden_door", () -> new BlockItem(UMUBlocks.GREEN_WOODEN_DOOR.get(), new Item.Properties().group(UMUItemGroups.FURNITURE)));
-	public static final RegistryObject<Item> RED_WOODEN_DOOR = ITEMS.register("red_wooden_door", () -> new BlockItem(UMUBlocks.RED_WOODEN_DOOR.get(), new Item.Properties().group(UMUItemGroups.FURNITURE)));
-	public static final RegistryObject<Item> BLACK_WOODEN_DOOR = ITEMS.register("black_wooden_door", () -> new BlockItem(UMUBlocks.BLACK_WOODEN_DOOR.get(), new Item.Properties().group(UMUItemGroups.FURNITURE)));
 
 	public static final RegistryObject<Item> WOODEN_BOX = ITEMS.register("wooden_box", () -> new BlockItem(UMUBlocks.WOODEN_BOX.get(), new Item.Properties().group(ItemGroup.DECORATIONS)));
 	public static final RegistryObject<Item> QUARTZ_COLUMN = ITEMS.register("quartz_column", () -> new BlockItem(UMUBlocks.QUARTZ_COLUMN.get(), new Item.Properties().group(ItemGroup.DECORATIONS)));
